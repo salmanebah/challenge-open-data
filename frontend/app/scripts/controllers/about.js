589d9fc70e96a1dcce7d1cf4fa8aad468d2f3f91
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2016-01-24 15:29:23 +0100

    dev-frontend: Add initial files

---
+++ frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name challengeOpenDataApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the challengeOpenDataApp
+ */
+angular.module('challengeOpenDataApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name challengeOpenDataApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the challengeOpenDataApp
+ */
+angular.module('challengeOpenDataApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
