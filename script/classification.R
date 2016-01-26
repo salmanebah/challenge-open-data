8c64763968b4fe28af4aa1c28a12356aaca4ddd5
Toibibou Houmadi <toibibou.houmadi@ensimag.grenoble-inp.fr>
2016-01-26 13:02:04 +0100

    Estimate the number of clusters and update classification.json

--- script/classification.R
+++ script/classification.R
@@ -15,18 +15,7 @@ diploma <- diploma[order(diploma$Code),]
 # Get code and region names
 regions_2016.names <- crimes[, 1:2]
 
-#delete comma in the age matrix.
-#age <- as.matrix(age)
-#for(i in 1:dim(age)[2]) {
-#  age[, i] <- gsub(",", "", age[, i])
-#}
-
-# delete "total" column
-#tmp <- age[,-dim(age)]
-
-
 # Add crimes, unemployment, pib and diploma. 
-
 regions_2016 <- cbind(regions_2016.names, age[,-c(1,2,ncol(age))], crimes$`2009`, unemployment$`2009`, pib$`2009`, diploma$`2006`)
 
 # Add column names
@@ -34,29 +23,23 @@ colnames(regions_2016)[8:11]<-c("crimes", "chomage", "pib", "diplome")
 
 
 #clustering
-
+library(fpc)
 regions_2016.use <- regions_2016[,3:ncol(regions_2016)]
 regions_2016.use <- na.omit(regions_2016.use) # listwise deletion of missing
-#regions_2016.use <- as.matrix(regions_2016.use); # transform to character matrix. 
-#for(i in 3:ncol(regions_2016)) {
- # regions_2016[, i] <- scale(as.numeric(regions_2016[, i])) # standardize variables
-#}
-# standardize variables
-regions_2016.use = scale(regions_2016.use)
+regions_2016.use = scale(regions_2016.use) # standardize variables
 
 # Hierarchical Agglomerative
-
 regions_2016.dist <- dist(regions_2016.use, method="euclidean")
 regions_2016.hclust <- hclust(regions_2016.dist, method = "average")
-#fit labels
-regions_2016.hclust$labels <- regions_2016.names$Code
+regions_2016.hclust$labels <- regions_2016.names$Code #add hclust labels
 
 plot(regions_2016.hclust, hang = -0.01, cex=0.7, labels = regions_2016.names$`Regions-2016`, 
      xlab="Classification des région en 2009")
-
-groups <- cutree(regions_2016.hclust, k=6) # cut tree into 5 cluster
+pamk.best <- pamk(regions_2016.dist) # Compute pam to estimate the number of clusters
+numberCluster <- pamk.best$nc
+groups <- cutree(regions_2016.hclust, k=numberCluster) # cut tree into k clusters
 # draw dendogram with red borders around the 5 clusters
-rect.hclust(regions_2016.hclust, k=6, border="red") 
+rect.hclust(regions_2016.hclust, k=numberCluster, border="red") 
 
 # Write result
 library(jsonlite)
@@ -79,13 +62,6 @@ for(class in keys(grp)) {
 classes <- list_regions 
 tmp <- list(year=2009, criteria=criteria, classes=c(classes))
 write(toJSON(tmp, pretty = TRUE, auto_unbox = TRUE), file = "../mongoDB-init/classification_2009.json")
-
-
-result <- function(groups, regions, criteria) {
-   
-}
-
-
 
 
 
