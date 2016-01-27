ca4a1170deff724ae77cf8cb145044d6d2389fd2
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2016-01-27 22:43:15 +0100

    dev-frontend: Retrieve new regions map from service

--- frontend/app/scripts/controllers/classification.js
+++ frontend/app/scripts/controllers/classification.js
@@ -7,7 +7,16 @@
  * # ClassificationCtrl
  * Controller of the challengeOpenDataApp
  */
+
 angular.module('challengeOpenDataApp')
     .controller('ClassificationCtrl', function ($scope, ClassificationService) {
-	$scope.hello = ClassificationService.getHello();
+	
+	// Get the map and bind it to mapInfo
+	ClassificationService.getMap().then(function(mapInfo) {
+	    // Setup the map
+	    $scope.newRegions = mapInfo.newRegions;
+	    // Setup the default options
+	    $scope.defaults = mapInfo.defaults;
+	    // Auto discover the center
+	});
   });
