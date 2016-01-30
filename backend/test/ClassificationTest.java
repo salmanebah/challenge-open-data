287dd0c25c4c57c263c3dd8e960cb5e953bb47b3
Amadou Sall <ahasall@yahoo.fr>
2016-01-30 10:25:19 +0100

    add test for the Classification DAO

--- backend/test/ClassificationTest.java
+++ backend/test/ClassificationTest.java
@@ -1,5 +1,22 @@
-/**
- * Created by ahasall on 28/01/16.
- */
+
+import models.classification.Classification;
+import models.classification.ClassificationDAO;
+import models.classification.Criteria;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertTrue;
+
 public class ClassificationTest {
+    @Test
+    public void testFindByCriteria() {
+        Criteria criteria = new Criteria(0, 0, 1, 1, 0);
+        Classification classification = ClassificationDAO.findByCriteria(criteria);
+        assertThat(classification, is(notNullValue()));
+    }
+
+
 }
