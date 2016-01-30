94dc9fd78630c8f8e8206cfbb34f01f98abac747
Toibibou Houmadi <toibibou.houmadi@ensimag.grenoble-inp.fr>
2016-01-31 00:46:35 +0100

    Implements a classifyRegions function
    	   This function allow to make a classification by choosing
    	   the criteria to consider.

--- script/classification.R
+++ script/classification.R
@@ -20,87 +20,78 @@ for(year in as.character(years)) {
   age[[year]] <- RemoveComma(age[[year]])
 }
 
-#Sorts regions according to their code
-crimes <- crimes[order(crimes$Code),]
-unemployment <- unemployment[order(unemployment$Code),]
-gdp <- gdp[order(gdp$Code),]
-diploma <- diploma[order(diploma$Code),]
-
+# #Sorts regions according to their code
+# crimes <- crimes[order(crimes$Code),]
+# unemployment <- unemployment[order(unemployment$Code),]
+# gdp <- gdp[order(gdp$Code),]
+# diploma <- diploma[order(diploma$Code),]
+# 
 # Get code and region names
-regions.code  <- crimes[, 1]
-regions.names <- crimes[, 2]
+ regions.code  <- crimes[, 1]
+ regions.names <- crimes[, 2]
+# 
+# # Builds a 3d matrix containing all the time series dim(1) corresponds to time 
+# # dim(2) the regions and dim(3) the variables. 
+# age.names <- names(age[['2009']][,-c(1,2, 8)])
+# variableNames <- c(age.names, "crimes", "unemployment", "GDP", "diploma")
+# regions.data <- array(dim = c(length(years), length(regions.names), length(variableNames)),
+#                            dimnames = list(years, regions.code, variableNames))
+# 
+# for (year in as.character(years)) {
+#   regions.data[year, ,'0 à 19 ans'] <- age[[year]][,'0 à 19 ans']
+#   regions.data[year, ,'20 à 39 ans'] <- age[[year]][,'20 à 39 ans']
+#   regions.data[year, ,'40 à 59 ans'] <- age[[year]][,'40 à 59 ans']
+#   regions.data[year, ,'60 à 74 ans'] <- age[[year]][,'60 à 74 ans']
+#   regions.data[year, ,'75 ans et plus'] <- age[[year]][,'75 ans et plus']
+#   
+#   if (!is.null(crimes[[year]])) {
+#     regions.data[year, , 'crimes'] <- crimes[[year]]
+#   }
+#   
+#   if (!is.null(unemployment[[year]])) {
+#     regions.data[year, , 'unemployment'] <- unemployment[[year]]
+#   }
+#   
+#   if (!is.null(gdp[[year]])) {
+#     regions.data[year, , 'GDP'] <- gdp[[year]]
+#   }
+#   
+#   if (!is.null(diploma[[year]])) {
+#     regions.data[year, , 'diploma'] <- diploma[[year]]
+#   }
+# }
+# 
+# # Clustering
+# library(fpc)
+# library(TSclust)
+# 
+# # Hierarchical Clustering based on similarity of permutation distributions
+# regions.pdclust <- pdclust(regions.data, clustering.method = "average")
+# plot(regions.pdclust, hang = -0.01, cex=0.7, xlab="Classification des région en 2009", 
+#      timeseries.as.labels = FALSE, labels = regions.names)
+# 
+# # Estimates the best number of clusters
+# pamk.best <- pamk(regions.pdclust$D, krange = 3:10) 
+# regions.nbclust <- pamk.best$nc
+# 
+# # Cuts tree into nbclust clusters
+# regions.groups <- cutree(regions.pdclust, k = regions.nbclust)
+# 
+# # Draws dendogram with red borders around the nbclust clusters
+# rect.hclust(regions.pdclust, k=regions.nbclust, border="red") 
+# 
+# 
+# # Writes result in JSON
+# ClassificationToJSON(regions, filename = "classification.json")
+# 
+# 
+# 
+# 
+# 
+# 
 
-# Builds a 3d matrix containing all the time series dim(1) corresponds to time 
-# dim(2) the regions and dim(3) the variables. 
-age.names <- names(age[['2009']][,-c(1,2, 8)])
-variableNames <- c(age.names, "crimes", "unemployment", "GDP", "diploma")
-regions.data <- array(dim = c(length(years), length(regions.names), length(variableNames)),
-                           dimnames = list(years, regions.code, variableNames))
+res <- ClassifyRegions(regions, crime = crimes, unemployment = unemployment, gbp = gdp, years = seq(1990, 2015), cluster.min = 3, 
+                            cluster.max = 10)
 
-for (year in as.character(years)) {
-  regions.data[year, ,'0 à 19 ans'] <- age[[year]][,'0 à 19 ans']
-  regions.data[year, ,'20 à 39 ans'] <- age[[year]][,'20 à 39 ans']
-  regions.data[year, ,'40 à 59 ans'] <- age[[year]][,'40 à 59 ans']
-  regions.data[year, ,'60 à 74 ans'] <- age[[year]][,'60 à 74 ans']
-  regions.data[year, ,'75 ans et plus'] <- age[[year]][,'75 ans et plus']
-  
-  if (!is.null(crimes[[year]])) {
-    regions.data[year, , 'crimes'] <- crimes[[year]]
-  }
-  
-  if (!is.null(unemployment[[year]])) {
-    regions.data[year, , 'unemployment'] <- unemployment[[year]]
-  }
-  
-  if (!is.null(gdp[[year]])) {
-    regions.data[year, , 'GDP'] <- gdp[[year]]
-  }
-  
-  if (!is.null(diploma[[year]])) {
-    regions.data[year, , 'diploma'] <- diploma[[year]]
-  }
-}
-
-# Add crimes, unemployment, gdp and diploma. 
-#regions_2016 <- cbind(regions_2016.names, age$`2009`[,-c(1,2,ncol(age$`2009`))], crimes$`2009`, unemployment$`2009`, gdp$`2009`, diploma$`2006`)
-
-# Add column names
-#colnames(regions_2016)[8:11]<-c("crimes", "unemployment", "gdp", "diplome")
-
-#clustering
-library(fpc)
-library(TSclust)
-
-#regions_2016.use <- regions_2016[,3:ncol(regions_2016)]
-#regions_2016.use <- na.omit(regions_2016.use) # listwise deletion of missing
-#regions_2016.use = scale(regions_2016.use) # standardize variables
-
-# Hierarchical Clustering based on similarity of permutation distributions
-regions.pdclust <- pdclust(regions.data, clustering.method = "average")
-
-#regions_2016.dist <- dist(regions_2016.use, method="euclidean")
-#regions_2016.hclust <- hclust(regions_2016.dist, method = "average")
-#regions_2016.hclust$labels <- regions_2016.names$Code #add hclust labels
-
-plot(regions.pdclust, hang = -0.01, cex=0.7, xlab="Classification des région en 2009", 
-     timeseries.as.labels = FALSE, labels = regions.names)
-
-# Estimates the best number of clusters
-pamk.best <- pamk(regions.pdclust$D, krange = 3:10) 
-regions.nbclust <- pamk.best$nc
-
-# Cuts tree into nbclust clusters
-regions.groups <- cutree(regions.pdclust, k = regions.nbclust)
-
-# Draws dendogram with red borders around the nbclust clusters
-rect.hclust(regions.pdclust, k=regions.nbclust, border="red") 
-
-
-# Writes result in JSON
-ClassificationToJSON(regions, filename = "classification.json")
-
-
-
-
-
+print(res)
 
--- script/util.R
+++ script/util.R
@@ -13,7 +13,7 @@ RemoveComma <- function(x, from = 3) {
 }
 
 
-ClassificationToJSON <- function(regions, use.crime = TRUE, use.age = TRUE, use.diploma = TRUE, 
+ClassificationToJSON <- function(regions.code, regions.names, regions.groups, use.crime = TRUE, use.age = TRUE, use.diploma = TRUE, 
                                  use.gdp = TRUE, use.unemployment = TRUE, 
                                  path = "../mongoDB-init/", filename = "classification") {
   # Writes the result of the classification in JSON
@@ -59,6 +59,105 @@ ClassificationToJSON <- function(regions, use.crime = TRUE, use.age = TRUE, use.
   
   # Converts to JSON and writes the result to a file. 
   regions.jsonResult <- toJSON(tmp, pretty = TRUE, auto_unbox = TRUE)
-  write(regions.jsonResult, file = paste(path, filename, sep = ""))
+  return(regions.jsonResult)
+  #write(regions.jsonResult, file = paste(path, filename, sep = ""))
 }
 
+# Clustering
+ClassifyRegions <- function(regions, crime = NULL, age = NULL, unemployment = NULL, gbp = NULL, 
+                            diploma = NULL, years = seq(1990, 2015), cluster.min = 3, 
+                            cluster.max = 10) {
+  
+  # Checks the required libraries are loaded. 
+  for (package in c('fpc', 'TSclust')) {
+    if (!require(package, character.only=T, quietly=T)) {
+      install.packages(package)
+      library(package, character.only=T)
+    }
+  }
+  
+  #Sorts regions according to their code
+  if (!is.null(crime)) {
+    variableNames <- c("crimes")
+    crimes <- crimes[order(crimes$Code),]
+  }
+  
+  if (!is.null(unemployment)) {
+    variableNames <- c(variableNames, "unemployment")
+    unemployment <- unemployment[order(unemployment$Code),]
+  }
+  
+  if (!is.null(gdp)) {
+    variableNames <- c(variableNames, "GDP")
+    gdp <- gdp[order(gdp$Code),]
+  }
+  
+  if (!is.null(diploma)) {
+    variableNames <- c(variableNames, "diploma")
+    diploma <- diploma[order(diploma$Code),]
+  }
+  
+  if(!is.null(age)) {
+    age.names <- names(age[['2009']][,-c(1,2, 8)])
+    variableNames <- c(variableNames, age.names)
+  }
+
+  # Builds a 3d matrix containing all the time series dim(1) corresponds to time 
+  # dim(2) the regions and dim(3) the variables. 
+
+  regions.data <- array(dim = c(length(years), length(regions.names), length(variableNames)),
+                        dimnames = list(years, regions.code, variableNames))
+  # Fills the matrix by year. 
+  for (year in as.character(years)) {
+    if(!is.null(age[[year]])) {
+      regions.data[year, ,'0 à 19 ans'] <- age[[year]][,'0 à 19 ans']
+      regions.data[year, ,'20 à 39 ans'] <- age[[year]][,'20 à 39 ans']
+      regions.data[year, ,'40 à 59 ans'] <- age[[year]][,'40 à 59 ans']
+      regions.data[year, ,'60 à 74 ans'] <- age[[year]][,'60 à 74 ans']
+      regions.data[year, ,'75 ans et plus'] <- age[[year]][,'75 ans et plus'] 
+    }
+    
+    if (!is.null(crimes[[year]])) {
+      regions.data[year, , 'crimes'] <- crimes[[year]]
+    }
+    
+    if (!is.null(unemployment[[year]])) {
+      regions.data[year, , 'unemployment'] <- unemployment[[year]]
+    }
+    
+    if (!is.null(gdp[[year]])) {
+      regions.data[year, , 'GDP'] <- gdp[[year]]
+    }
+    
+    if (!is.null(diploma[[year]])) {
+      regions.data[year, , 'diploma'] <- diploma[[year]]
+    }
+  }
+  
+  # Hierarchical Clustering based on similarity of permutation distributions
+  regions.pdclust <- pdclust(regions.data, clustering.method = "average")
+  plot(regions.pdclust, hang = -0.01, cex = 0.7, xlab = paste("Classification selon le ", 
+                                                              variableNames, collapse = ", "), 
+       timeseries.as.labels = FALSE, labels = regions.names)
+  
+  # Estimates the best number of clusters
+  pamk.best <- pamk(regions.pdclust$D, krange = cluster.min:cluster.max) 
+  regions.nbclust <- pamk.best$nc
+  
+  # Cuts tree into nbclust clusters
+  regions.groups <- cutree(regions.pdclust, k = regions.nbclust)
+  
+  # Draws dendogram with red borders around the nbclust clusters
+  rect.hclust(regions.pdclust, k=regions.nbclust, border="red") 
+  
+  # Writes result in JSON
+   result <- ClassificationToJSON(regions.code = regions.code, regions.names = regions.names,
+                                  regions.groups = regions.groups,
+                                  filename    = "classification.json", 
+                                  use.crime   = ifelse(is.null(crime), FALSE, TRUE), 
+                                  use.age     = ifelse(is.null(age), FALSE, TRUE), 
+                                  use.diploma = ifelse(is.null(diploma), FALSE, TRUE),
+                                  use.gdp     = ifelse(is.null(gbp), FALSE, TRUE),
+                                  use.unemployment = ifelse(is.null(unemployment), FALSE, TRUE))
+   return(result)
+}
