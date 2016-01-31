29b149cff21183b294bec1d05718f3e0bf7967fe
Toibibou Houmadi <toibibou.houmadi@ensimag.grenoble-inp.fr>
2016-02-01 00:01:43 +0100

    Update the mongodb database schema
           Add a classification score and  medoid (cluster representative individual)

--- mongoDB-init/classification.js
+++ mongoDB-init/classification.js
@@ -9,19 +9,40 @@ db.classifications.insert(
         "population" : 0
     },
     "year" : 2015,
+	"score" : 0.765
     "classes": [
         {
+			"medoid": {
+        		"id": 11,
+        		"name": "Nord-Pas-de-Calais-Picardie",
+        		"unemployment": 7.775,
+        		"GDP":1165173
+      		}
+
             "regions": [
                 {"id" : 11,"name" : "Ile de France"}
             ]
         },
         {
+			"medoid": {
+        		"id": 28,
+        		"name": "Nord-Pas-de-Calais-Picardie",
+        		"unemployment": 8.775,
+        		"GDP": 65112
+      		}
+
             "regions": [
                 {"id" : 27,"name" : "Bourgogne-Franche-Comté"},
                 {"id" : 28,"name" : "Normandie"},
             ]
         },
         {
+			"medoid": {
+        		"id": 32,
+        		"name": "Nord-Pas-de-Calais-Picardie",
+        		"unemployment": 9.775,
+        		"GDP": 65173
+      		}
             "regions": [
                 {"id" : 32,"name" : "Nord-Pas-de-Calais-Picardie"},
             ]
