becf1b59997ba4a19732c253fec221321956686d
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2016-02-03 16:19:57 +0100

    Refactor correlation service

--- frontend/app/scripts/services/correlation.js
+++ frontend/app/scripts/services/correlation.js
@@ -8,43 +8,55 @@
  * Service in the challengeOpenDataApp.
  */
 angular.module('challengeOpenDataApp')
-  .service('CorrelationService', function () {
+    .service('CorrelationService', function (CorrelationData) {
+	var yearsData = CorrelationData.years;
+	console.log(yearsData);
       	var d3Service = {};
+
 	var margin = {top : 20, right: 20, bottom: 30, left: 40};
 	var width = 1000 - margin.left - margin.right;
 	var height = 600 - margin.top - margin.bottom;
 	var padding = 30;
+	var svg;
+	var chart;
+	var xScale;
+	var yScale;
+	var rScale;
+	var xAxis;
+	var yAxis;
+	var circles;
 
-	var svg = d3.select("#bubble-chart").append("svg")
-		.attr("height", height + margin.left + margin.right)
-		.attr("width", width + margin.top + margin.bottom);
-
-	var chart = svg.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-	d3Service.drawChart = function(data) {
+	var getDataByCriteria = function(year, criteria) {
+	    //TODO: validate input.
+	    var yearInfo = yearsData[year];
+	    console.log(yearInfo);
 	    
-	    var xScale = d3.scale.linear()
+	};
+
+	var initSvg = function() {
+	    svg = d3.select("#bubble-chart").append("svg")
+		    .attr("height", height + margin.left + margin.right)
+		    .attr("width", width + margin.top + margin.bottom);
+	    
+	    chart = svg.append("g")
+		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	};
+
+	var updateScale = function(data) {
+	    xScale = d3.scale.linear()
 		    .domain([0, d3.max(data, function(d) { return d.xCriterion.value; })])
 		    .range([padding,  width - padding * 2]);
 	    
-	    var yScale = d3.scale.linear()
+	    yScale = d3.scale.linear()
 	  	    .domain([0, d3.max(data, function(d) { return d.yCriterion.value; })])
 		    .range([height - padding,  padding]);
-
-	    var rScale = d3.scale.linear()
+	    
+	    rScale = d3.scale.linear()
 		    .domain([0, d3.max(data, function(d) { return d.sizeCriterion.value; })])
 		    .range([4, 15]);
-	    
-	    var xAxis = d3.svg.axis()
-		    .scale(xScale)
-		    .orient("bottom");
+	};
 
-	    var yAxis = d3.svg.axis()
-		    .scale(yScale)
-		    .orient("left");
-
+	var updateXaxis = function(data) {
 	    chart.selectAll(".x.axis").transition().duration(1500).call(xAxis);
 	    chart.selectAll("text.xlabel").text("");
 
@@ -58,10 +70,12 @@ angular.module('challengeOpenDataApp')
 		.attr("y", -6)
 		.style("text-anchor", "end")
 		.text(data[0].xCriterion.name); // set xCriterion name as label
+	};
 
+	var updateYaxis = function(data) {
 	    chart.selectAll(".y.axis").transition().duration(500).call(yAxis);
 	    chart.selectAll("text.ylabel").text("");
-
+	    
 	    chart.append("g")
 		.attr("class", "y axis")
 		.attr("transform", "translate(0," + padding + ",0)")
@@ -73,11 +87,25 @@ angular.module('challengeOpenDataApp')
 		.attr("dy", ".71em")
 		.style("text-anchor", "end")
 		.text(data[0].yCriterion.name);
+	};
 
+	var updateAxis = function(data) {
+	    xAxis = d3.svg.axis()
+		.scale(xScale)
+		.orient("bottom");	    
+	    yAxis = d3.svg.axis()
+		.scale(yScale)
+		.orient("left");
+	    updateXaxis(data);
+	    updateYaxis(data);
+	};
+
+
+	var drawRegions = function(data) {
 	    var duration = 1500;
 	    var delay = 7;
 	    //load the data
-	    var circles = chart.selectAll("circle")
+	    circles = chart.selectAll("circle")
 		    .data(data, function(d) { return d.region.id; });
 	    circles
 		.transition()
@@ -96,8 +124,16 @@ angular.module('challengeOpenDataApp')
 		.attr("r", 0)
 		.transition()
 		.attr("r", function(d)  { return rScale(d.sizeCriterion.value);})	  
-	    //TODO: update the color according to the region
-		.style("fill", "blue");
+	        //TODO: update the color according to the region
+		.style("fill", "blue");  
+	};
+
+	// Called once to setup the context
+	initSvg();
+	d3Service.drawChart = function(data) {
+	    updateScale(data);
+	    updateAxis(data);
+	    drawRegions(data);	  
 	};
 	
 	return d3Service;
