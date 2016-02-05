340ac5e71bd198653c7b9242c6012dff51e94264
Amadou Sall <ahasall@yahoo.fr>
2016-02-05 01:36:44 +0100

    update frontend

--- frontend/app/scripts/controllers/classification.js
+++ frontend/app/scripts/controllers/classification.js
@@ -13,7 +13,7 @@
 
 
 angular.module('challengeOpenDataApp')
-  .controller('ClassificationCtrl', ['$scope', '$http', 'leafletData', function ($scope, $http) {
+  .controller('ClassificationCtrl', ['$scope', '$http', function ($scope, $http) {
 
     var maxbounds = {
       northEast: {
@@ -30,18 +30,38 @@ angular.module('challengeOpenDataApp')
       lng: -2,
       zoom: 6
     };
+    var colors = [
+      'black',
+      '#01665e',
+      '#8e0152',
+      '#4d9221',
+      '#40004b',
+      '#92c5de',
+      '#b2182b',
+      '#053061'
+    ];
     var criteria = {
       'crime': 0,
-      'education': 0,
-      'employment': 0,
+      'age': 0,
+      'diploma': 0,
       'gdp': 0,
-      'population': 0
+      'unemployment': 0
     };
-    var clusters = [];
-    var clusters2015 = [];
-    var colors = ['black', 'yellow', 'green', 'brown', 'red', 'pink'];
+    var getYears = function (criteria) {
+      for (var i in $scope.yearsData) {
+        if (JSON.stringify(criteria) === JSON.stringify($scope.yearsData[i].criteria)) {
+          console.log($scope.yearsData[i].years);
+          console.log($scope.yearsData[i].years);
+          return $scope.yearsData[i].years;
+        }
+      }
+    };
 
     function init() {
+      $http.get('/data/years.json')
+        .success(function (data) {
+          $scope.yearsData = data;
+        });
       angular.extend($scope, {
         defaults: {
           scrollWheelZoom: true,
@@ -50,19 +70,35 @@ angular.module('challengeOpenDataApp')
         maxbounds: maxbounds,
         france: france,
         criteria: criteria,
-        clusters: clusters,
-        clusters2015 : clusters2015,
+        clusters: [],
+        clusters2015: [],
         colors: colors,
         geojson: {
           data: [],
           style: function (feature) {
-            return $scope.getStyle(feature);
+            return $scope.getStyle(feature, $scope.clusters);
           }
         },
-        geojson2015 : {
-          data : []
+        geojson2015: {
+          data: [],
+          style: function (feature) {
+            return $scope.getStyle(feature, $scope.clusters2015);
+          }
         },
-        year : 2006
+        year: 2006,
+        years: getYears(criteria),
+        score: 0,
+        score2015: 0,
+        legend: {
+          position: 'bottomleft',
+          colors: ['#ff0000', '#28c9ff', '#0000ff', '#ecf386'],
+          labels: ['National Cycle Route', 'Regional Cycle Route', 'Local Cycle Network', 'Cycleway']
+        },
+        legend2015: {
+          position: 'bottomleft',
+          colors: ['#ff0000', '#28c9ff', '#0000ff', '#ecf386'],
+          labels: ['National Cycle Route', 'Regional Cycle Route', 'Local Cycle Network', 'Cycleway']
+        }
       });
 
       $scope.layer = L.geoJson();
@@ -79,9 +115,9 @@ angular.module('challengeOpenDataApp')
     }
 
     init();
-    $scope.getStyle = function (feature) {
+    $scope.getStyle = function (feature, clusters) {
       return {
-        fillColor: $scope.getColor(feature),
+        fillColor: $scope.getColor(feature.properties.code, clusters),
         weight: 1,
         opacity: 1,
         color: 'black',
@@ -93,16 +129,18 @@ angular.module('challengeOpenDataApp')
     $scope.updateColors = function () {
       var empty = {
         'crime': 0,
-        'education': 0,
-        'employment': 0,
+        'age': 0,
+        'diploma': 0,
         'gdp': 0,
-        'population': 0
+        'unemployment': 0
       };
       if ((count % 2 ) === 0) {
         console.log('red');
         $scope.geojson.style = {fillColor: 'red'};
+        $scope.geojson2015.style = {fillColor: 'red'};
       } else if ((count % 2) === 1) {
         $scope.geojson.style = {fillColor: 'blue'};
+        $scope.geojson2015.style = {fillColor: 'blue'};
         console.log('blue');
       }
       count++;
@@ -110,17 +148,24 @@ angular.module('challengeOpenDataApp')
       if (JSON.stringify(empty) !== JSON.stringify($scope.criteria)) {
         $http.post('/api/classification/' + $scope.year, $scope.criteria)
           .success(function (data) {
-            $scope.clusters = data.clusters;
+            $scope.clusters = data.newClassification.clusters;
+            $scope.score = data.newClassification.score;
+            $scope.clusters2015 = data.oldClassification.clusters;
+            $scope.score2015 = data.oldClassification.score;
+
             $scope.geojson.style = function (feature) {
-              return $scope.getStyle(feature);
+              return $scope.getStyle(feature, $scope.clusters);
+            };
+            $scope.geojson2015.style = function (feature) {
+              return $scope.getStyle(feature, $scope.clusters2015);
             };
           });
       }
     };
 
 // get the adequate color according to the cluster that contains the region
-    $scope.getColor = function (feature) {
-      var clusterId = $scope.getClusterId(feature.id);
+    $scope.getColor = function (featureId, clusters) {
+      var clusterId = $scope.getClusterId(featureId, clusters);
       if (clusterId >= 0) {
         return $scope.colors[clusterId];
       }
@@ -128,9 +173,9 @@ angular.module('challengeOpenDataApp')
     };
 
 // Get the cluster in which the region is
-    $scope.getClusterId = function (featureId) {
-      for (var i in $scope.clusters) {
-        var cluster = $scope.clusters[i];
+    $scope.getClusterId = function (featureId, clusters) {
+      for (var i in clusters) {
+        var cluster = clusters[i];
         if (cluster.hasOwnProperty('regions')) {
           for (var region in cluster.regions) {
             if (parseInt(cluster.regions[region].id) === parseInt(featureId)) {
@@ -143,11 +188,11 @@ angular.module('challengeOpenDataApp')
     };
     $scope.$watch('criteria', function () {
       console.log('criteria has changed');
-      console.log(criteria);
+      $scope.years = getYears(criteria)
     }, true);
     $scope.$watch('year', function () {
       console.log('year has changed');
-      console.log(year);
+      console.log($scope.year);
     }, true);
     /*
      function mouseOverHandler(event) {
