3174c6da415bfb85074ec860721aaa5153f62e34
Amadou Sall <ahasall@yahoo.fr>
2016-02-05 01:34:39 +0100

    update correlation

--- frontend/app/scripts/controllers/correlation.js
+++ frontend/app/scripts/controllers/correlation.js
@@ -15,11 +15,15 @@ angular.module('challengeOpenDataApp')
       {name: "unemployment", index: 1},
       {name: "diploma", index: 2},
       {name: "gdp", index: 3},
-      {name: "age", index: 4}
+      {name: "0 à 19 ans", index: 4},
+      {name: "20 à 39 ans", index: 5},
+      {name: "40 à 59 ans", index: 6},
+      {name: "60 à 74 ans", index: 7},
+      {name: "75 ans et plus", index: 8}
     ];
 
-    $scope.year = {};
-    $scope.years = [];
+    $scope.year = "2004";
+    $scope.years = [2004];
     var drawer = CorrelationService.drawChart;
 
 
@@ -30,15 +34,42 @@ angular.module('challengeOpenDataApp')
 
     var getYears = function () {
       var criteria = {
-        "crime": false,
-        "unemployment": false,
-        "gdp": false,
-        "age": false,
-        "diploma": false
+        'crime': 0,
+        'age': 0,
+        'diploma': 0,
+        'gdp': 0,
+        'unemployment': 0
       };
-      criteria[$scope.currentCriterion[0].name] = true;
-      criteria[$scope.currentCriterion[1].name] = true;
-      criteria[$scope.currentCriterion[2].name] = true;
+
+      if (($scope.currentCriterion[0].name === "0 à 19 ans") ||
+        ($scope.currentCriterion[0].name === "20 à 39 ans") ||
+        ($scope.currentCriterion[0].name === "40 à 59 ans") ||
+        ($scope.currentCriterion[0].name === "60 à 74 ans") ||
+        ($scope.currentCriterion[0].name === "75 ans et plus")) {
+        criteria['age'] = 1;
+      } else {
+        criteria[$scope.currentCriterion[0].name] = 1;
+      }
+      if (($scope.currentCriterion[1].name === "0 à 19 ans") ||
+        ($scope.currentCriterion[1].name === "20 à 39 ans") ||
+        ($scope.currentCriterion[1].name === "40 à 59 ans") ||
+        ($scope.currentCriterion[1].name === "60 à 74 ans") ||
+        ($scope.currentCriterion[1].name === "75 ans et plus")) {
+        criteria['age'] = 1;
+      } else {
+        criteria[$scope.currentCriterion[1].name] = 1;
+      }
+      if (($scope.currentCriterion[2].name === "0 à 19 ans") ||
+        ($scope.currentCriterion[2].name === "20 à 39 ans") ||
+        ($scope.currentCriterion[2].name === "40 à 59 ans") ||
+        ($scope.currentCriterion[2].name === "60 à 74 ans") ||
+        ($scope.currentCriterion[2].name === "75 ans et plus")) {
+        criteria['age'] = 1;
+      } else {
+        criteria[$scope.currentCriterion[2].name] = 1;
+      }
+
+
       for (var i in $scope.yearsData) {
         if (JSON.stringify(criteria) === JSON.stringify($scope.yearsData[i].criteria)) {
           console.log($scope.yearsData[i].years);
@@ -58,28 +89,25 @@ angular.module('challengeOpenDataApp')
       var i = getIndex($scope.currentCriterion[pos].index, 1);
       $scope.currentCriterion[pos] = criterion[i];
       $scope.years = getYears();
+      $scope.year = $scope.years[0];
       updateDraw();
     };
     $scope.previous = function (pos) {
       var i = getIndex($scope.currentCriterion[pos].index, -1);
       $scope.currentCriterion[pos] = criterion[i];
       $scope.years = getYears();
+      $scope.year = $scope.years[0];
       updateDraw();
     };
 
-    var updateDraw2 = function () {
-      var data = CorrelationService.getDataByCriteria("2004",
-        $scope.currentCriterion[0].name, $scope.currentCriterion[0].name, $scope.currentCriterion[0].name);
-      drawer(data);
-    };
     var updateDraw = function () {
-      var data = CorrelationService.getDataByCriteria("2004",
-        "crime",
-        "GDP",
-        "unemployment");
+      var data = CorrelationService.getDataByCriteria($scope.year,
+        $scope.currentCriterion[0].name, $scope.currentCriterion[1].name, $scope.currentCriterion[2].name);
       drawer(data);
     };
-    updateDraw2();
+    $scope.$watch('year', function () {
+      updateDraw();
+    });
 
   });
 
